491f23dd9b333c09a88786b1e906c459cd395ee3
Ashikuzzaman Abir <ashikuzzamanabir@outlook.com>
2023-03-14 03:34:20 +0600

    update

--- components/Consultation.jsx
+++ components/Consultation.jsx
@@ -9,7 +9,7 @@ function Consultation() {
 			<p className=' mt-2 text-center w-[514px] mx-auto text-[24px] font-[400] tracking-[1px]'>
 				Let us help you choose the best products for you and your loved one.
 			</p>
-			<div className='mt-8 cursor-pointer w-fit bg-black text-white mx-auto px-8 py-2 text-[14px] font-[500] tracking-[2px]'>GET FREE CONSULTATION</div>
+			<div className='mt-8 cursor-pointer w-fit bg-black text-white mx-auto px-8 py-4 text-[14px] font-[500] tracking-[2px]'>GET FREE CONSULTATION</div>
 		</div>
 	);
 }
